9b9b6d61c2c066bd8f24013bfa142d2f46f7da99
vladislav <vladislavPozhar@yandex.ru>
2024-08-25 22:08:49 +0400

    Написал интеграционный тест на стороне клиента

---
+++ manager-app/src/test/java/own/eteryz/controller/ProductsControllerIT.java
@@ -0,0 +1,39 @@
+package own.eteryz.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductsControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+
+    void getNewProductPage_ReturnsProductPage() throws Exception {
+        // given
+        // Можно добиться с помощью аннотации @WithMockUser()
+        var requestBuilder = MockMvcRequestBuilders.get("/catalogue/products/create")
+                .with(user("vlad").roles("MANAGER"));
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("catalogue/products/create")
+                );
+
+    }
+}
