be298d52fb6a2b70ef40be438b32fc25dd6d4676
Vladislav <VladislavPozhar@yandex.ru>
2024-09-21 20:06:17 +0400

    Добавил запрос через Query

--- feedback-service/src/main/java/own/eteryz/entity/FavouriteProduct.java
+++ feedback-service/src/main/java/own/eteryz/entity/FavouriteProduct.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document("favourite_product")
 public class FavouriteProduct {
 
     @Id
--- feedback-service/src/main/java/own/eteryz/repository/ProductReviewRepository.java
+++ feedback-service/src/main/java/own/eteryz/repository/ProductReviewRepository.java
@@ -1,5 +1,6 @@
 package own.eteryz.repository;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import own.eteryz.entity.ProductReview;
 import reactor.core.publisher.Flux;
@@ -8,5 +9,6 @@ import java.util.UUID;
 
 public interface ProductReviewRepository extends ReactiveCrudRepository<ProductReview, UUID> {
 
+    @Query("{'productId':  ?0}")
     Flux<ProductReview> findAllByProductId(int productId);
 }
