42a3de6c979b4a1fc84dba3749252fe19f8ebe27
Vladislav <VladislavPozhar@yandex.ru>
2024-09-22 01:08:06 +0400

    Настроил keycloak для customer-app

--- customer-app/src/main/java/own/eteryz/customer/config/ClientConfig.java
+++ customer-app/src/main/java/own/eteryz/customer/config/ClientConfig.java
@@ -3,6 +3,10 @@ package own.eteryz.customer.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 import own.eteryz.customer.client.WebClientFavouriteProductClient;
 import own.eteryz.customer.client.WebClientProductReviewsClient;
@@ -12,28 +16,43 @@ import own.eteryz.customer.client.WebClientProductsClient;
 public class ClientConfig {
 
     @Bean
-    public WebClientProductsClient webClientProductsClient(
-            @Value("${eteryz.services.catalog.url:http://localhost:8081}") String baseUrl
+    @Scope("prototype")
+    public WebClient.Builder eteryzServicesWebClientBuilder(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository
     ) {
-        return new WebClientProductsClient(WebClient.builder()
+        var filter = new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                clientRegistrationRepository, authorizedClientRepository);
+        filter.setDefaultClientRegistrationId("keycloak");
+        return WebClient.builder().filter(filter);
+    }
+
+    @Bean
+    public WebClientProductsClient webClientProductsClient(
+            @Value("${eteryz.services.catalog.url:http://localhost:8081}") String baseUrl,
+            WebClient.Builder eteryzServicesWebClientBuilder
+    ) {
+        return new WebClientProductsClient(eteryzServicesWebClientBuilder
                 .baseUrl(baseUrl)
                 .build());
     }
 
     @Bean
     public WebClientProductReviewsClient webClientProductReviewsClient(
-            @Value("${eteryz.services.feedback.url:http://localhost:8084}") String baseUrl
+            @Value("${eteryz.services.feedback.url:http://localhost:8084}") String baseUrl,
+            WebClient.Builder eteryzServicesWebClientBuilder
     ) {
-        return new WebClientProductReviewsClient(WebClient.builder()
+        return new WebClientProductReviewsClient(eteryzServicesWebClientBuilder
                 .baseUrl(baseUrl)
                 .build());
     }
 
     @Bean
     public WebClientFavouriteProductClient webClientFavouriteProductsClient(
-            @Value("${eteryz.services.feedback.url:http://localhost:8084}") String baseUrl
+            @Value("${eteryz.services.feedback.url:http://localhost:8084}") String baseUrl,
+            WebClient.Builder eteryzServicesWebClientBuilder
     ) {
-        return new WebClientFavouriteProductClient(WebClient.builder()
+        return new WebClientFavouriteProductClient(eteryzServicesWebClientBuilder
                 .baseUrl(baseUrl)
                 .build());
     }
