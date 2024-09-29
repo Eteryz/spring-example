5796c8742d9b70b37261dbe48fc78d1da727fbcb
Vladislav <VladislavPozhar@yandex.ru>
2024-09-30 00:49:12 +0400

    Зарегистрировал manager-app в eureka

--- manager-app/src/main/java/own/eteryz/config/ClientBeans.java
+++ manager-app/src/main/java/own/eteryz/config/ClientBeans.java
@@ -1,6 +1,8 @@
 package own.eteryz.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -15,13 +17,15 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClientImpl productsRestClient(
-            @Value("${catalog.service.baseUrl:http://localhost:8080/api/v1}") String catalogBaseUrl,
+            @Value("${catalog.service.baseUrl:http://localhost:8080}/api/v1") String catalogBaseUrl,
             ClientRegistrationRepository clientRegistrationRepository,
             OAuth2AuthorizedClientRepository authorizedClientRepository,
-            @Value("${catalog.service.registration-id:keycloak}") String registrationId
+            @Value("${catalog.service.registration-id:keycloak}") String registrationId,
+            LoadBalancerClient loadBalancerClient
     ) {
         return new RestClientProductsRestClientImpl(
                 RestClient.builder()
+                        .requestInterceptor(new LoadBalancerInterceptor(loadBalancerClient))
                         .baseUrl(catalogBaseUrl)
                         .requestInterceptor(
                                 new OAuthClientRequestInterceptor(
